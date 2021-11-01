49957169919536e915aabfe7226374538772cc46
Miguel Grinberg <miguel.grinberg@gmail.com>
2021-11-01 12:03:13 +0000

    First release

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # twilio-functions-storage
-Storage object compatible with the Web Storage API for Twilio serverless functions.
+Storage object for Twilio serverless functions that is compatible with the Web Storage API.
+
+## Example
+
+```javascript
+const Storage = require('twilio-functions-storage');
+
+exports.handler = async function(context, event, callback) {
+  // create a storage object
+  const storage = Storage(context);
+
+  // get a variable
+  const test = storage.getItem('test');
+
+  // set a variable
+  storage.setItem('foo', 'bar');
+
+  // delete a variable
+  storage.removeItem('old');
+
+  // save() must be called before exiting to preserve changes
+  await storage.save();
+
+  callback();
+}
+```
+
+## Implementation Notes
+
+Internally, this package uses [Environment Variable](https://www.twilio.com/docs/runtime/functions-assets-api/api/variable)
+resources to permanently store data. All the variables created by this packaage
+begin with the prefix `S_`.
+
+### Limits
+
+The following are the limitations of this implementation:
+
+- Item values cannot be longer than 450 bytes.
+- The Twilio Runtime allocate approximately 3KB for environment variable storage.
---
+++ index.js
@@ -0,0 +1,79 @@
+const Storage = (context) => {
+  let vars = {};
+
+  if (context.SERVICE_SID) {
+    for (variable in context) {
+      if (variable.startsWith('S_')) {
+        vars[variable.substring(2)] = context[variable];
+      }
+    }
+  }
+  else {
+    vars = require('dotenv').config({ path: 'storage.env' }).parsed;
+  }
+
+  return {
+    get length() {
+      return Object.keys(vars).length;
+    },
+
+    key: (n) => {
+      return Object.keys(vars)[n];
+    },
+
+    getItem: (keyname) => {
+      return vars[keyname];
+    },
+
+    setItem: (keyname, value) => {
+      vars[keyname] = value;
+    },
+
+    removeItem: (keyname) => {
+      delete vars[keyname];
+    },
+
+    clear: () => {
+      vars = {};
+    },
+
+    save: async() => {
+      if (context.SERVICE_SID) {
+        const resource = context.getTwilioClient().serverless
+          .services(context.SERVICE_SID)
+          .environments(context.ENVIRONMENT_SID)
+          .variables;
+        const currentVariables = await resource.list();
+        for (variable in vars) {
+          const remoteVariable = 'S_' + variable;
+          const existingVariable = currentVariables.find(v => v.key == remoteVariable);
+          if (existingVariable) {
+            await existingVariable.update({ value: `${vars[variable]}` });
+          }
+          else {
+            await resource.create({ key: remoteVariable, value: `${vars[variable]}` });
+          }
+        }
+        for (let i = 0; i < currentVariables.length; i++) {
+          const existingVariable = currentVariables[i];
+          if (!existingVariable.key.startsWith('S_')) {
+            continue;
+          }
+          const variable = existingVariable.key.substring(2);
+          if (vars[variable] === undefined) {
+            await existingVariable.remove();
+          }
+        }
+      }
+      else {
+        env = '';
+        for (variable in vars) {
+          env += `${variable}=${vars[variable]}\n`;
+        }
+        await require('fs').promises.writeFile('storage.env', env);
+      }
+    },
+  };
+};
+
+module.exports = Storage;
